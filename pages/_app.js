d08b35b010f67a0e09d6ffe223e1de8464daef33
Jay Toy <jay_toyjianying@hotmail.com>
2022-10-24 13:41:27 +0200

    add google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,29 @@
-import { Provider } from 'react-redux';
-import Footer from '../components/Footer';
-import Header from '../components/Header';
-import store from '../redux/store';
-import '../styles/globals.css'
+import { Provider } from "react-redux";
+import Script from "next/script";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+import store from "../redux/store";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
     <Provider store={store}>
+      {/* Google tag (gtag.js) */}
+      <Script
+        strategy="lazyOnload"
+        src="https://www.googletagmanager.com/gtag/js?id=G-K73BZPLFLQ"
+      />
+      <Script id="google-analytics" strategy="lazyOnload">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+  
+          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+            page_path: window.location.pathname,
+          });
+        `}
+      </Script>
       <div>
         <Header />
         <Component {...pageProps} />
@@ -16,4 +33,4 @@ function MyApp({ Component, pageProps }) {
   );
 }
 
-export default MyApp
+export default MyApp;
